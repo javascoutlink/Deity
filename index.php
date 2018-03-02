938e6559ca69a9a3fb14179f229ace03f574ec79
java <ladbrookm@gmail.com>
2018-03-03 12:47:55 +1300

    Images
    Added images of the church to the images > all folder

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Avonhead Baptist Church || Home Page</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div class="header">
+    </div>
+    <div class="body">
+      <div class="jumbotron parallax">
+        <h1>Avonhead Baptist</h1>
+        <h2>Discipling the Nations accross the Ages</h2>
+      </div>
+    </div
+  </body>
+</html>
