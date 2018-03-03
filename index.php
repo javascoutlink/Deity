836bb902a47f4e5d8d63649cd358f3f4283a1e2c
Java <matthew@ladbrook.tk>
2018-03-03 17:34:30 +1300

    Menu
    HTML and CSS code added to create the top menu bar.

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
   </head>
   <body>
     <div class="header">
+      <div class="menu" id="menu-left">
+        <a href="#">Home</a>
+        <a href="contact.php">Contact</a>
+      </div>
+      <div class="menu" id="menu-right">
+        <a href="#">Ministries</a>
+        <a href="contact.php">Contact</a>
+      </div>
     </div>
     <div class="body">
       <div id="jumbotron" class="background">
         <h1>Avonhead Baptist</h1>
         <h2>Discipling the Nations accross the Ages</h2>
       </div>
+    </div>
+    <p>Some text</p>
   </body>
 </html>
