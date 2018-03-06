31bd6a4c406dc58054767370edc83d71b4d4ede4
Java <matthew@ladbrook.tk>
2018-03-07 09:04:50 +1300

    Added about us page
    Created an about us page.

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Avonhead Baptist Church || About Us</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="include.js"></script>
+  </head>
+  <body>
+    <div w3-include-html="menu.html"></div>
+    <div class="body">
+      <div id="jumbotron" class="background">
+        <h1>About Us</h1>
+        <h2>Who were we, who are we and what will be become</h2>
+      </div>
+    </div>
+  </body>
+</html>
+<script>
+includeHTML();
+</script>
