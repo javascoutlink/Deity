7d5174054ff280fbdbaba8b2864005700d6607c1
Java <matthew@ladbrook.tk>
2018-03-06 17:45:52 +1300

    Added ministry.php page
    Added a page for information about church ministry.

---
+++ ministry.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Avonhead Baptist Church || Ministry</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <style>
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <div class="menu" id="menu-left">
+        <a href="members.php">Members</a>
+        <a href="contact.php">Contact</a>
+      </div>
+      <a href="index.php">
+        <div class="menu-logo">
+        </div>
+      </a>
+      <div class="menu" id="menu-right">
+        <a href="ministry.php">Ministries</a>
+        <a href="about.php">About Us</a>
+      </div>
+    </div>
+    <div class="body">
+      <div id="jumbotron" class="background">
+        <h1>Ministry</h1>
+        <h2>The <b>Hands</b> of the church</h2>
+      </div>
+    </div>
+  </body>
+</html>
