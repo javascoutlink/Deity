5fd82344df90d5b33547f162550b8af2b061d830
Java <matthew@ladbrook.tk>
2018-03-07 09:05:05 +1300

    Added a contact page
    Created a standard contact page

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Avonhead Baptist Church || Contact</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="include.js"></script>
+  </head>
+  <body>
+    <div w3-include-html="menu.html"></div>
+    <div class="body">
+      <div id="jumbotron" class="background">
+        <h1>Contact</h1>
+        <h2>We want to hear from You</h2>
+      </div>
+    </div>
+  </body>
+</html>
+<script>
+includeHTML();
+</script>
