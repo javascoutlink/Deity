b90e6466e654d048cbff3da6ceb62ef745bfa48b
Java <matthew@ladbrook.tk>
2018-03-07 09:01:21 +1300

    Added default page
    Created a default template page for new pages.

---
+++ default.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Avonhead Baptist Church || Description</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="include.js"></script>
+  </head>
+  <body>
+    <div w3-include-html="menu.html"></div>
+    <div class="body">
+      <div id="jumbotron" class="background">
+        <h1>Headline</h1>
+        <h2>Tagline</h2>
+      </div>
+    </div>
+  </body>
+</html>
+<script>
+includeHTML();
+</script>
