40ecc2a72649249e7a4585a6d90269df5c37bdf2
Java <matthew@ladbrook.tk>
2018-03-08 11:56:08 +1300

    Added team page
    Information about individual team members.

--- team.php
+++ team.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Avonhead Baptist Church || Contact</title>
+    <title>Avonhead Baptist Church || Description</title>
     <link href="style.css" rel="stylesheet" type="text/css">
     <script src="include.js"></script>
   </head>
@@ -9,12 +9,10 @@
     <div w3-include-html="menu.html"></div>
     <div class="body">
       <div id="jumbotron" class="background">
-        <h1>Contact</h1>
-        <h2>We want to hear from You</h2>
-      </div>
-      <div id="meet" class="background secondary">
         <h1>Meet the Team</h1>
-        <a href="team.php"><button class="link-button">Find out more</button></a>
+        <h2>Tagline</h2>
+      </div>
+      <div id="team" class="text-box">
       </div>
     </div>
   </body>
