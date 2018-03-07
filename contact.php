021d3c0bc61a607014d9438c84735c80366f750d
Java <matthew@ladbrook.tk>
2018-03-08 11:56:26 +1300

    Contact Page updates

--- contact.php
+++ contact.php
@@ -3,6 +3,15 @@
   <head>
     <title>Avonhead Baptist Church || Contact</title>
     <link href="style.css" rel="stylesheet" type="text/css">
+    <style>
+    #jumbotron {
+      background-image: url('images/contact.jpg');
+    }
+
+    #meet {
+      background-image: url('images/jumbotron.jpg');
+    }
+    </style>
     <script src="include.js"></script>
   </head>
   <body>
@@ -11,6 +20,21 @@
       <div id="jumbotron" class="background">
         <h1>Contact</h1>
         <h2>We want to hear from You</h2>
+      </div>
+      <h1 class="tb-head">Contact the Main Office</h1>
+      <div id="maincontact" class="text-box">
+        <div id="phone">
+          <h1>Phone</h1>
+          <h2>348 6742</h2>
+        </div>
+        <div id="email">
+          <h1>Email</h1>
+          <h2>olive@avonheadbaptist.org</h2>
+        </div>
+        <div id="mail">
+          <h1>Post</h1>
+          <h2>102 Avonhead Road</h2>
+        </div>
       </div>
       <div id="meet" class="background secondary">
         <h1>Meet the Team</h1>
